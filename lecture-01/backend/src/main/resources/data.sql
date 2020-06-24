b7921f677a805b677ab3b619c35de8c44615a032
kihwankim <clack2933@gmail.com>
2020-06-25 05:54:11 +0900

    add h2 database and make sql for testing

---
+++ lecture-01/backend/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into todo(id, username, description, target_date, is_done)
+values (1001,'kkh', 'learn Jpa', sysdate(), false);
+
+insert into todo(id, username, description, target_date, is_done)
+values (1002,'abc', 'learn Spring', sysdate(), false);
+
+insert into todo(id, username, description, target_date, is_done)
+values (1003,'bcd', 'learn Microservice', sysdate(), false);
